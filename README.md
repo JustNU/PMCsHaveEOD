ce1a142a3cdf2afd0f42a5a51e7d359ad72bbc1f
JustNU <ivancool108@gmail.com>
2023-01-23 04:32:28 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PMC Bots Have EOD Accounts
+PMC Bots not have EOD account, which makes them show up with yellow names if you get killed by them.
---
+++ src/mod.js
@@ -0,0 +1,15 @@
+﻿"use strict";
+
+class Mod {
+	postDBLoad(container) {
+		// constants
+		const logger = container.resolve("WinstonLogger");
+		const database = container.resolve("DatabaseServer").getTables();
+		
+		// wow so much lines
+		database.bots.base.Info.MemberCategory = 2;
+	}
+}
+
+	
+module.exports = { mod: new Mod() }
